fa55b315c55b9d9870429ef21d3adf7f87561ff1
unknown <n.d.cesar@gmail.com>
2019-10-14 17:22:05 +0200

    PrimerPush

---
+++ js/saludo.js
@@ -0,0 +1,9 @@
+//alert("Hola mundo");
+//confirm("Mensaje con confirmación");
+//prompt("¿Cuál es tu nombre?");
+
+document.write("escribiendo en la pagina web directamente");
+
+console.log("Esto es un mensaje de consola");
+console.error("UN ERROR DIOH MIO");
+console.warn("UNA ADVERTENCIA TEN CUIDAOH");
