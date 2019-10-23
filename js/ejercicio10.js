45cd420fa7f2dbc808d1ebce113b03b00a992282
unknown <n.d.cesar@gmail.com>
2019-10-23 23:26:34 +0200

    Ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,19 @@
+function modificarfecha() {
+
+  var fechaActual = new Date();
+
+  var dias = fechaActual.getDate();
+  var operacion = document.getElementById("operacion").value;
+  var comp = document.getElementById("r1").value;
+  parseInt(comp);
+
+  if (comp == 1) {
+    dias += parseInt(operacion);
+  } else if (comp == 2) {
+    dias -= parseInt(operacion);
+  }
+
+  fechaActual.setDate(dias);
+
+  document.getElementById("demo").innerHTML = fechaActual;
+}
