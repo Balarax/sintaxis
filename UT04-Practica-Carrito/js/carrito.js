6674afda187bba7610d1c0de167f44b444bfbe1b
unknown <n.d.cesar@gmail.com>
2019-12-02 00:40:08 +0100

    Practica Carrito version final

--- UT04-Practica-Carrito/js/carrito.js
+++ UT04-Practica-Carrito/js/carrito.js
@@ -1,4 +1,4 @@
-class carrito {
+class Carrito {
 	constructor(id_carrito, items, cantidades) {
 
 		this.id_carrito = id_carrito;
@@ -42,7 +42,7 @@ class Producto {
 		return this._num_serie;
 	}
 	set num_serie(value) {
-		this._num_serie = value;
+		this._num_serie = "S/N:" + value;
 	}
 	get nombre() {
 		return this._nombre;
@@ -69,10 +69,12 @@ class Producto {
 }
 
 // Object.defineProperties(Producto, {
-
-// 	"num_serie":{get: function(){ return this.num_serie;}}
-	
+// 	"num_serie": { get: function () { return this.num_serie; } },
+// 	"num_serie": { set: function (value) { this.num_serie = value; } },
+// 	"nombre": { get: function () { return this.nombre; } },
+// 	"nombre": { set: function (value) { this.nombre = value; } }
 // });
+
 class Camiseta extends Producto {
 	constructor(num_serie, nombre, precio, iva, talla, color) {
 		super(num_serie, nombre, precio, iva);
@@ -162,7 +164,7 @@ class Zapatilla extends Producto {
 	}
 }
 
-var carritocompra = new carrito;
+var carritocompra = new Carrito(1, [], []);
 
 function Anadir(precio, nombre) {
 
@@ -182,82 +184,468 @@ function Anadir(precio, nombre) {
 
 }
 
+/*
+Funcion para validar los campos Y AÑADIR LOS ARTICULOS
+@version 1.0
+*/
+
+function ValidarCampos() {
+
+	var tipo = document.getElementById('Selector').value;
+
+	switch (tipo) {
+		case 'Camiseta':
+			var num_serie = document.getElementById('Num_serie').value;
+			var nombre = document.getElementById('Nombre').value;
+			var precio = document.getElementById('Precio').value;
+			//var iva = document.getElementById('Iva').value;
+			var talla = document.getElementById('Talla').value;
+			var color = document.getElementById('Color').value;
+			var cantidad = document.getElementById('Cantidad').value;
+
+
+			try {
+				var re = new RegExp('^\\d{8}$');
+				console.log(re);
+				var re1 = new RegExp('^(XS|S|M|L|XL|XXL|XXXL)$');
+				console.log(re1);
+
+				if (num_serie == "") {
+					throw "ERROR, el numero de serie es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (nombre == "") {
+					throw "ERROR, el nombre es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (talla == "") {
+					throw "ERROR, la talla es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (!num_serie.match(re)) {
+					throw "ERROR, el numero de serie debe contener 8 digitos";
+				}
+
+				// if (iva != 10) {
+				// 	throw "ERROR, el iva para este articulo solo puede ser 10%";
+				// }
+				if (!talla.match(re1)) {
+					throw "ERROR, las tallas disponibles son: XS | S | M | L | XL | XXL | XXXL";
+				}
+
+			} catch (error) {
+				document.getElementById('ControlDeErrores').style.color = "red";
+				document.getElementById('ControlDeErrores').innerHTML = error;
+				break;
+			}
+
+			if (precio == "") {
+				precio = 0;
+			}
+			if (cantidad == "") {
+				cantidad = 1;
+			}
+
+			var Article = new Camiseta;
+			Article.num_serie = num_serie;
+			Article.nombre = nombre;
+			Article.iva = 10;
+			Article.precio = precio;
+			Article.talla = talla;
+			Article.color = color;
+
+			carritocompra.items.push(Article);
+			carritocompra.cantidades.push(cantidad);
+
+			document.getElementById('ControlDeErrores').style.color = "green";
+			document.getElementById('ControlDeErrores').innerHTML = "Articulo Añadido correctamente";
+
+
+			break;
+
+		case 'Pantalon':
+
+			var num_serie = document.getElementById('Num_serie').value;
+			var nombre = document.getElementById('Nombre').value;
+			var precio = document.getElementById('Precio').value;
+			// var iva = document.getElementById('Iva').value;
+			var talla = document.getElementById('Talla').value;
+			var color = document.getElementById('Color').value;
+			var ancho = document.getElementById('Ancho').value;
+			var largo = document.getElementById('Largo').value;
+			var cantidad = document.getElementById('Cantidad').value;
+
+			try {
+				var re = new RegExp('^\\d{8}$');
+				console.log(re);
+				var re1 = new RegExp('^(XS|S|M|L|XL|XXL|XXXL)$');
+				console.log(re1);
+				var re2 = new RegExp('^([3][2-9]|[4][0-8])$');
+				console.log(re2);
+				var re3 = new RegExp('^([2][8-9]|[3-4][0-9]|[5][0-4])$');
+				console.log(re3);
+
+				if (num_serie == "") {
+					throw "ERROR, el numero de serie es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (nombre == "") {
+					throw "ERROR, el nombre es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (talla == "") {
+					throw "ERROR, la talla es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (!num_serie.match(re)) {
+					throw "ERROR, el numero de serie debe contener 8 digitos";
+				}
+
+				// if (iva!=10) {
+				// 	throw "ERROR, el iva para este articulo solo puede ser 10%";
+				// }
+
+				if (!talla.match(re1)) {
+					throw "ERROR, las tallas disponibles son: XS | S | M | L | XL | XXL | XXXL";
+				}
+				if (ancho != "") {
+
+					if (!ancho.match(re2)) {
+						throw "ERROR, el ancho debe ser un numero y estar entre 32 y 48";
+					}
+
+				}
+
+				if (largo != "") {
+
+					if (!largo.match(re3)) {
+						throw "ERROR, el largo debe ser un numero y estar entre 28 y 54";
+					}
+				}
+
+			} catch (error) {
+				document.getElementById('ControlDeErrores').style.color = "red";
+				document.getElementById('ControlDeErrores').innerHTML = error;
+				break;
+			}
+
+			if (precio == "") {
+				precio = 0;
+			}
+			if (ancho == "") {
+				ancho = 0;
+			}
+			if (largo == "") {
+				largo = 0;
+			}
+			if (cantidad == "") {
+				cantidad = 1;
+			}
+
+			var Article = new Pantalon;
+			Article.num_serie = num_serie;
+			Article.nombre = nombre;
+			Article.iva = 10;
+			Article.precio = precio;
+			Article.talla = talla;
+			Article.color = color;
+			Article.ancho = ancho;
+			Article.largo = largo;
+
+			carritocompra.items.push(Article);
+			carritocompra.cantidades.push(cantidad);
+
+			document.getElementById('ControlDeErrores').style.color = "green";
+			document.getElementById('ControlDeErrores').innerHTML = "Articulo Añadido correctamente";
+
+			break;
+
+		case 'Zapatilla':
+			var num_serie = document.getElementById('Num_serie').value;
+			var nombre = document.getElementById('Nombre').value;
+			var precio = document.getElementById('Precio').value;
+			// var iva = document.getElementById('Iva').value;
+			var numero = document.getElementById('Numero').value;
+			var tipo = document.getElementById('Tipo').value;
+			var cantidad = document.getElementById('Cantidad').value;
+
+			try {
+				var re = new RegExp('^\\d{8}$');
+				console.log(re);
+				var re1 = new RegExp('^(EU|USA|UK|CM)$');
+				console.log(re1);
+				var re2 = new RegExp('^([1][6-9]|[2-3][0-9]|[4][0-8])$');
+				console.log(re2);
+
+				if (num_serie == "") {
+					throw "ERROR, el numero de serie es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (nombre == "") {
+					throw "ERROR, el nombre es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (numero == "") {
+					throw "ERROR, el numero es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (tipo == "") {
+					throw "ERROR, el tipo es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (!num_serie.match(re)) {
+					throw "ERROR, el numero de serie debe contener 8 digitos";
+				}
+
+				// if (iva!=10) {
+				// 	throw "ERROR, el iva para este articulo solo puede ser 10%";
+				// }
+
+
+				if (!numero.match(re2)) {
+					throw "ERROR, el numero debe ser un numero y estar entre 16 y 48";
+				}
+
+				if (!tipo.match(re1)) {
+					throw "ERROR, los tipos disponibles son: EU | USA | UK | CM ";
+				}
+
+			} catch (error) {
+				document.getElementById('ControlDeErrores').style.color = "red";
+				document.getElementById('ControlDeErrores').innerHTML = error;
+				break;
+			}
+
+			if (precio == "") {
+				precio = 0;
+			}
+			if (cantidad == "") {
+				cantidad = 1;
+			}
+
+			var Article = new Zapatilla;
+			Article.num_serie = num_serie;
+			Article.nombre = nombre;
+			Article.iva = 4;
+			Article.precio = precio;
+			Article.numero = numero;
+			Article.tipo = tipo;
+
+			carritocompra.items.push(Article);
+			carritocompra.cantidades.push(cantidad);
+
+			document.getElementById('ControlDeErrores').style.color = "green";
+			document.getElementById('ControlDeErrores').innerHTML = "Articulo Añadido correctamente";
+			break;
+
+		case 'Producto':
+
+			var num_serie = document.getElementById('Num_serie').value;
+			var nombre = document.getElementById('Nombre').value;
+			var precio = document.getElementById('Precio').value;
+			var iva = document.getElementById('Iva').value;
+			var cantidad = document.getElementById('Cantidad').value;
+
+			try {
+				var re = new RegExp('^\\d{8}$');
+				console.log(re);
+				var re1 = new RegExp('^([0-9]|[1][0-9]|[2][01])$');
+				console.log(re1);
+
+				if (num_serie == "") {
+					throw "ERROR, el numero de serie es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (nombre == "") {
+					throw "ERROR, el nombre es un campo OBLIGATORIO y no puede estar vacío";
+				}
+
+				if (!num_serie.match(re)) {
+					throw "ERROR, el numero de serie debe contener 8 digitos";
+				}
+				if (iva != "") {
+
+					if (!iva.match(re1)) {
+						throw "ERROR, el iva no puede ser superior al 21%";
+					}
+				}
+
+
+			} catch (error) {
+				document.getElementById('ControlDeErrores').style.color = "red";
+				document.getElementById('ControlDeErrores').innerHTML = error;
+				break;
+			}
+
+			if (precio == "") {
+				precio = 0;
+			}
+
+			if (iva == "") {
+				iva = 0;
+			}
+
+			if (cantidad == "") {
+				cantidad = 1;
+			}
+
+			var Article = new Producto;
+			Article.num_serie = num_serie;
+			Article.nombre = nombre;
+			Article.iva = iva;
+			Article.precio = precio;
+
+			carritocompra.items.push(Article);
+			carritocompra.cantidades.push(cantidad);
+
+			document.getElementById('ControlDeErrores').style.color = "green";
+			document.getElementById('ControlDeErrores').innerHTML = "Articulo Añadido correctamente";
+
+			break;
+
+		default:
+
+			pintar += "<h4> Por favor, seleccione un articulo </h4>";
+
+			break;
+	}
+
+}
+
+/*
+Funcion que genera la interfaz según lo que hayamos elegido en el desplegable
+@version 1.0
+*/
+function GenerarCampos() {
+
+	var tipo = document.getElementById('Selector').value;
+	var pintar = "";
+
+	switch (tipo) {
+		case 'Camiseta':
+			pintar += "<p>Numero de serie(Obligatorio): <input type='text' id='Num_serie'></p>";
+			pintar += "<p>Nombre(Obligatorio): <input type='text' id='Nombre'></p>";
+			pintar += "<p>Precio: <input type='number' id='Precio'></p>";
+			// pintar += "<p>Iva: <input type='number' id='Iva'></p>";
+			pintar += "<p>Talla(Obligatorio): <input type='text' id='Talla'></p>";
+			pintar += "<p>Color: <input type='text' id='Color'></p>";
+			pintar += "<p>Cantidad: <input type='text' id='Cantidad'></p>";
+			break;
+		case 'Pantalon':
+			pintar += "<p>Numero de serie(Obligatorio): <input type='text' id='Num_serie'></p>";
+			pintar += "<p>Nombre(Obligatorio): <input type='text' id='Nombre'></p>";
+			pintar += "<p>Precio: <input type='number' id='Precio'></p>";
+			// pintar += "<p>Iva: <input type='number' id='Iva'></p>";
+			pintar += "<p>Talla(Obligatorio): <input type='text' id='Talla'></p>";
+			pintar += "<p>Color: <input type='text' id='Color'></p>";
+			pintar += "<p>Ancho: <input type='text' id='Ancho'></p>";
+			pintar += "<p>Largo: <input type='text' id='Largo'></p>";
+			pintar += "<p>Cantidad: <input type='text' id='Cantidad'></p>";
+
+			break;
+		case 'Zapatilla':
+			pintar += "<p>Numero de serie(Obligatorio): <input type='text' id='Num_serie'></p>";
+			pintar += "<p>Nombre(Obligatorio): <input type='text' id='Nombre'></p>";
+			pintar += "<p>Precio: <input type='number' id='Precio'></p>";
+			// pintar += "<p>Iva: <input type='number' id='Iva'></p>";
+			pintar += "<p>Numero(Obligatorio): <input type='text' id='Numero'></p>";
+			pintar += "<p>Tipo(Obligatorio): <input type='text' id='Tipo'></p>";
+			pintar += "<p>Cantidad: <input type='text' id='Cantidad'></p>";
+			break;
+		case 'Producto':
+			pintar += "<p>Numero de serie(Obligatorio): <input type='text' id='Num_serie'></p>";
+			pintar += "<p>Nombre(Obligatorio): <input type='text' id='Nombre'></p>";
+			pintar += "<p>Precio: <input type='number' id='Precio'></p>";
+			pintar += "<p>Iva: <input type='number' id='Iva'></p>";
+			pintar += "<p>Cantidad: <input type='text' id='Cantidad'></p>";
+			break;
+
+		default:
+
+			pintar += "<h4> Por favor, seleccione un articulo </h4>";
+
+			break;
+	}
+
+	document.getElementById('MostrarCampos').innerHTML = pintar;
+
+
+}
+
+/*
+Funcion que muestra los articulos que se han introducido en el carrito
+@version 1.0
+*/
 function MostrarCarrito() {
 
 	var pintar = "";
 
 	var cont = 0;
+	 var cont1 = 0;
+	 var cont2 = 0;
+	var TotalArticulos = 0;
+	var Totalprecio = 0;
+	 var PrecioSinIva = 0;
 
-	pintar += "<h4> Artículos en tu carrito: </h4>";
+	//Con esto mostramos el total de articulos en nuestro carrito.
+	for (x of carritocompra.cantidades) {
+		x = parseInt(x);
+		TotalArticulos += x;
+	}
 
-	carritocompra.listararticulos.forEach(element => {
+	carritocompra.items.forEach(element => {
+		element.precio = parseInt(element.precio);
+		Totalprecio += (element.precio*carritocompra.cantidades[cont2]);
+		  cont2++;
+	});
 
-		pintar += "<input type='radio' name=article id=" + cont + " value=" + cont + ">" + element.nombre + " - " + element.precio + "€<br>";
+	// Esta parte del codigo es para sacar los precios sin iva
+	carritocompra.items.forEach(element => {
+		element.precio = parseInt(element.precio);
+		element.iva = parseInt(element.iva);
+		PrecioSinIva += (element.precio*carritocompra.cantidades[cont1]) - (((element.precio * element.iva) / 100)*carritocompra.cantidades[cont1]);
+		cont1++;
+	});
+
+	pintar += "<h4> Artículos en tu carrito: " + TotalArticulos + " </h4>";
+	 pintar += "<h4> Precio sin IVA: " + PrecioSinIva + "€ </h4>";
+	pintar += "<h4> Precio total a pagar: " + Totalprecio + "€ </h4>";
+
+	carritocompra.items.forEach(element => {
+
+		pintar += "<input type='radio' name=article id=" + cont + " value=" + cont + ">" + element.num_serie + " - " + element.nombre + " - " + element.precio + "€ -- " + carritocompra.cantidades[cont] + " Unidades <br>";
 		cont++;
 	});
 
 	document.getElementById('mostrarcarrito').innerHTML = pintar;
 
 }
-
+/*
+Funcion que elimina los articulos marcados en el radiobutton generado en la funcion MostrarCarrito(), y a continuacion refresca la pantalla
+@version 1.0
+*/
 function EliminarArticulo() {
 
-	for (let i in carritocompra.listararticulos) {
+	for (let i in carritocompra.items) {
 
 		if (document.getElementById(i).checked) {
-
-			var preciores = carritocompra.listararticulos[Object.values(i)] //conseguimos los valores del objeto que se encuentra en la posicion i
-
-			console.log(preciores.precio);
-			carritocompra.unidades -= 1;
-			carritocompra.precio -= preciores.precio; //Le restamos al carrito el precio del objeto que hemos obtenido anteriormente
-
-			carritocompra.listararticulos.splice(i, 1);
+			carritocompra.items.splice(i, 1);
+			carritocompra.cantidades.splice(i, 1);
 		}
 	}
 
-
-	var pintar = "";
-
-	var cont = 0;
-
-	pintar += "<h4> Artículos en tu carrito: </h4>";
-
-	carritocompra.listararticulos.forEach(element => {
-
-		pintar += "<input type='radio' name=article id=" + cont + " value=" + cont + ">" + element.nombre + " - " + element.precio + "€<br>";
-		cont++;
-	});
-
-	document.getElementById('mainScreen').innerHTML = "Unidades: " + carritocompra.unidades + "<br>Precio: " + carritocompra.precio + " €"; //Refrescamos el carrito
-	document.getElementById('mostrarcarrito').innerHTML = pintar;
+	MostrarCarrito();
 }
+
+/*
+Funcion DE PRUEBA DEL EJERCICIO, no se puede utilizar, a menos que actives el boton test en el html
+@version 1.0
+*/
 
 function testExercise() {
 
-	var num_serie = 32423, nombre = "yoquese", precio = 23, iva = 21, talla = "XL", color = "azul";
+	console.log(carritocompra.items);
+	console.log(carritocompra.cantidades);
 
-
-	var camiseta = new Producto(num_serie, nombre, precio, iva);
-
-	//Validar(camiseta);
-
-	console.log(camiseta);
-
-
-	 var pintar = document.getElementById("pintar");
-	 pintar.innerHTML = camiseta.toString();
 
 }
-
-// function Validar(objeto) {
-// 	try {
-
-// 		if (condition) {
-
-// 		}
-
-// 	} catch (error) {
-
-// 	}
-// }
